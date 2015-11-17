917af2bf73be68ac2257b5e834708b0eaeccccd4
Jonas Otto <jottosmail@gmail.com>
2015-11-17 21:11:10 +0100

    merged login form to index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+	<body>
+		<h1>Index Page</h1>
+		<br>
+		<?php echo("PHP works"); ?>
+		<br>
+		<br>
+		<form action="./register.php">
+			<input type="submit" value="Register">
+		</form>
+		<form action="./displayHomework.php">
+			<input type="submit" value="Display Homework">
+		</form>
+		
+		
+		
+		
+		<div>
+		<form action="login.php" method="post">
+
+		email: <br>
+		<input type="text" name="email"> <br>
+
+		password: <br>
+		<input type="password" name="password"> <br>
+
+		<input type="submit" value="Log in">
+
+		</form>
+		</div>
+
+
+
+	</body>
+</html>
