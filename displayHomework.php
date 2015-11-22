c66ed38fdf748fe17b5d6a7cbc95c7c33d70be75
Jonas Otto <jottosmail@gmail.com>
2015-11-22 19:15:30 +0100

    displaying number of entries

--- displayHomework.php
+++ displayHomework.php
@@ -1,6 +1,34 @@
 <?php
 	
-	
-	
-	
+require("sqlconnection.php");
+
+if (isset($_COOKIE["uId"]) && isset($_COOKIE["uSessionKey"])) {
+
+    $uId = $_COOKIE["uId"];
+
+    $userData = $conn->query("SELECT uSessionKey, uFirstName, uLastName, uEmail FROM users WHERE uId = $uId");
+    $userData = $userData->fetch_array();
+    $uSessionKey = $userData["uSessionKey"];
+    $uFirstName = $userData["uFirstName"];
+
+    if (password_verify($_COOKIE["uSessionKey"], $uSessionKey)) {
+
+        #LOGGED IN
+        $homeworkCount = $conn->query("SELECT COUNT(`hId`) FROM `homework` WHERE `uId` = $uId");
+        $homeworkCount = $homeworkCount->fetch_array();
+        $homeworkCount = $homeworkCount["COUNT(`hId`)"];
+        echo("Welcome, $uFirstName.<br>");
+        echo("You have $homeworkCount Tasks created.");
+
+
+
+
+    } else {
+        header("Location: ./index.php");
+    }
+} else {
+    header("Location: ./index.php");
+}
+
+
 ?>
