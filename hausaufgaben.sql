c0a79cad745fe575b52d6bae0216c17bc682db69
Jonas Otto <Jonas Otto>
2015-11-24 16:24:30 +0100

    Added SQL File

--- hausaufgaben.sql
+++ hausaufgaben.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 4.1.12
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 17. Nov 2015 um 21:12
--- Server-Version: 5.6.26
--- PHP-Version: 5.6.12
+-- Erstellungszeit: 24. Nov 2015 um 16:24
+-- Server Version: 5.6.16
+-- PHP-Version: 5.5.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Datenbank: `hausaufgaben`
@@ -23,43 +23,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `homework`
+--
+
+CREATE TABLE IF NOT EXISTS `homework` (
+  `uId` int(11) NOT NULL,
+  `hId` int(11) NOT NULL AUTO_INCREMENT,
+  `bId` int(11) NOT NULL,
+  `hPageNr` int(11) NOT NULL,
+  `hExerciseNr` int(11) NOT NULL,
+  `hCreationTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `hDeadline` datetime NOT NULL,
+  `hSubject` text NOT NULL,
+  `hNotes` text NOT NULL,
+  PRIMARY KEY (`hId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `uId` int(11) NOT NULL,
+  `uId` int(11) NOT NULL AUTO_INCREMENT,
   `uFirstName` varchar(255) DEFAULT NULL,
   `uLastName` varchar(255) DEFAULT NULL,
   `uEmail` varchar(255) DEFAULT NULL,
-  `uHashedPassword` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `uHashedPassword` varchar(255) DEFAULT NULL,
+  `uSessionKey` varchar(255) NOT NULL,
+  PRIMARY KEY (`uId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Daten für Tabelle `users`
 --
 
-INSERT INTO `users` (`uId`, `uFirstName`, `uLastName`, `uEmail`, `uHashedPassword`) VALUES
-(1, 'Hans', 'Wurst', 'a@b.c', '$2y$10$iDxajeIPcXEhcDydBfQyZOl3cAbHJFCrkN64OqbD9o7v1NgVf/moG');
+INSERT INTO `users` (`uId`, `uFirstName`, `uLastName`, `uEmail`, `uHashedPassword`, `uSessionKey`) VALUES
+(1, 'Hans', 'Wurst', 'a@b.c', '$2y$10$iDxajeIPcXEhcDydBfQyZOl3cAbHJFCrkN64OqbD9o7v1NgVf/moG', ''),
+(2, 'Jonas', 'Otto', 'jottosmail@gmail.com', '$2y$10$baPK0amca8jlCD630y5MKuLc0YwqBl.TfyH67G7AvxFsAiAwHAh8u', '$2y$10$uYNY8vOgcz1QOdlrrDPETe2j2cy6wowQZyn0PgriXdlPGNsFu9AWa');
 
---
--- Indizes der exportierten Tabellen
---
-
---
--- Indizes für die Tabelle `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`uId`);
-
---
--- AUTO_INCREMENT für exportierte Tabellen
---
-
---
--- AUTO_INCREMENT für Tabelle `users`
---
-ALTER TABLE `users`
-  MODIFY `uId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
