f270a2ee6d19628dafd386aecbc45c4e0fed9794
Jonas Otto <Jonas Otto>
2015-12-15 16:57:23 +0100

    json object from GET data

--- getHelp.php
+++ getHelp.php
@@ -6,10 +6,23 @@
  * Time: 14:44
  */
 
-$bookId = $_POST["bookId"];
-$pageNr = $_POST["pageNr"];
-$exerciseNr = $_POST["exerciseNr"];
+$bookId = $_GET["bookId"];
+$pageNr = $_GET["pageNr"];
+$exerciseNr = $_GET["exerciseNr"];
 
 require("sqlconnection.php");
 
-$conn->query("SELECT uId FROM homework WHERE bId = $bookId && hPageNr = $pageNr && hExerciseNr = $exerciseNr");
+$result = $conn->query("SELECT homework.uId, homework.bId, homework.hPageNr, homework.hExerciseNr, users.uFirstName, users.uEmail FROM homework, users WHERE bId = $bId && hPageNr = $pageNr && hExerciseNr = $exerciseNr && homework.uId = users.uId ");
+
+
+
+
+while($row = $result->fetch_assoc())
+{
+	$uId = $row["uId"];
+	$post_data["bId"] = $row["bId"];
+	$post_data["hPageNr"] = $row["hPageNr"];
+	$post_data["hExerciseNr"] = $row["hExerciseNr"];
+	$object[$uId] = $post_data; 
+}
+echo json_encode($object);
